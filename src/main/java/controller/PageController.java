8f703c0f5cb59c14911addaf1b5f6788f3727454
saodoingoi1991 <dangky14031991@gmail.com>
2017-09-13 17:20:09 +0700

    first commit

---
+++ src/main/java/controller/PageController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PageController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
+    @RequestMapping("/home")
+    public String homePage(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        return "admin/home";
+    }
+
+}
---
+++ src/main/java/controller/SeedStarterMngController.java
@@ -0,0 +1,13 @@
+//package controller;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//public class SeedStarterMngController {
+////    @Autowired
+////    private VarietyService varietyService;
+//
+////    @Autowired
+////    private SeedStarterService seedStarterService;
+//}
